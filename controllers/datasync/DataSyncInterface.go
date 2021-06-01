2a0118ba5c5072ba836e963fa3ced43b497275a3
Micheal Akinwonmi <michealakinwonmi@gmail.com>
2021-06-01 21:53:49 +0100

    USSD Recievec Completed

--- controllers/datasync/DataSyncInterface.go
+++
@@ -1,8 +0,0 @@
-package datasync
-
-import "subscription-billing-engine/entities"
-
-type NetworkDataSyncInterface interface {
-	IngestByte([]byte) error
-	ProcessDataSync(string) (error, *entities.GenericDataSyncPayload)
-}
--- entities/Generic.go
+++ entities/Generic.go
@@ -5,23 +5,6 @@ const STATUS_PENDING = "pending"
 const STATUS_QUEUED = "queued"
 const STATUS_FAILED = "failed"
 
-type ChargeAuthorizationResponse struct {
-	//status can be success(call charge immediately), pending (), queued (It has been queued and we are waiting for feedback), failed( just failed)
-	Status    string      `json:"status"`
-	Reference string      `json:"reference"`
-	Error     string      `json:"error"`
-	Data      interface{} `json:"data"`
-}
-
-type GenericConsentResponse struct {
-	Network        string      `json:"network"`
-	Reference      string      `json:"reference"`
-	IsConsentGiven bool        `json:"isConsentGiven"`
-	Msisdn         string      `json:"msisdn"`
-	AuthToken      string      `json:"authToken"`
-	Meta           interface{} `json:"meta"`
-}
-
 type GenericChargeResponse struct {
 	Status    string      `json:"status"`
 	Reference string      `json:"reference"`
