85f5be073b55d977a01e71d80ca8e4be451eff84
Micheal Akinwonmi <michealakinwonmi@gmail.com>
2021-06-18 17:58:38 +0100

    Changed How we get service code

--- controllers/receive/implementation/ExchangeImplementation.go
+++ controllers/receive/implementation/ExchangeImplementation.go
@@ -4,24 +4,25 @@ import (
 	"encoding/xml"
 	"fmt"
 	"github.com/labstack/echo/v4"
+	"strings"
 	"ussd-router/entities"
 	"ussd-router/entities/exchange"
 )
 
 var messageTypeMap = map[string]string{
-	"0":  "begin",
+	"0": "begin",
 	"1": "continue",
 	"2": "end",
 }
 var operationTypeMap = map[string]string{
-	"1":  "request",
+	"1": "request",
 	"2": "notify",
 	"3": "response",
 	"4": "release",
 }
 
 type ExchangeReceiveImplementation struct {
-	Payload  exchange.USSDReceivePayload `json:"payload"`
+	Payload exchange.USSDReceivePayload `json:"payload"`
 }
 
 func (request *ExchangeReceiveImplementation) Validate() error {
@@ -46,30 +47,37 @@ func (request *ExchangeReceiveImplementation) Process(byteData []byte) (error, *
 		return err, nil
 	}
 
-	fmt.Println("USSD Receive From Exchange", ussdReceive)
-	return nil, &entities.GenericUSSDReceivePayload{
-		Provider: "exchange",
-		MessageType: messageTypeMap[ussdReceive.Body.USSDReceiveNotifyUSSDReceptionBody.MsgType],
-		SpId: ussdReceive.Header.NotifySoapHeader.SpId,
-		ServiceId: ussdReceive.Header.NotifySoapHeader.ServiceId,
-		Timestamp: ussdReceive.Header.NotifySoapHeader.TimeStamp,
-		USSDString: ussdReceive.Body.USSDReceiveNotifyUSSDReceptionBody.USSDString,
-		Msisdn: ussdReceive.Body.USSDReceiveNotifyUSSDReceptionBody.Msisdn,
-		CodeScheme: ussdReceive.Body.USSDReceiveNotifyUSSDReceptionBody.CodeScheme,
-		SessionId: ussdReceive.Body.USSDReceiveNotifyUSSDReceptionBody.SenderCB,
-		Reference: ussdReceive.Header.NotifySoapHeader.TraceUniqueId,
-		AccessCode: ussdReceive.Body.USSDReceiveNotifyUSSDReceptionBody.ServiceCode,
-		ServiceCode: ussdReceive.Body.USSDReceiveNotifyUSSDReceptionBody.ServiceCode,
+	response := &entities.GenericUSSDReceivePayload{
+		Provider:      "exchange",
+		MessageType:   messageTypeMap[ussdReceive.Body.USSDReceiveNotifyUSSDReceptionBody.MsgType],
+		SpId:          ussdReceive.Header.NotifySoapHeader.SpId,
+		ServiceId:     ussdReceive.Header.NotifySoapHeader.ServiceId,
+		Timestamp:     ussdReceive.Header.NotifySoapHeader.TimeStamp,
+		USSDString:    ussdReceive.Body.USSDReceiveNotifyUSSDReceptionBody.USSDString,
+		Msisdn:        ussdReceive.Body.USSDReceiveNotifyUSSDReceptionBody.Msisdn,
+		CodeScheme:    ussdReceive.Body.USSDReceiveNotifyUSSDReceptionBody.CodeScheme,
+		SessionId:     ussdReceive.Body.USSDReceiveNotifyUSSDReceptionBody.SenderCB,
+		Reference:     ussdReceive.Header.NotifySoapHeader.TraceUniqueId,
 		OperationType: operationTypeMap[ussdReceive.Body.USSDReceiveNotifyUSSDReceptionBody.UssdOpType],
 	}
+	if response.OperationType == "begin" {
+		fmt.Println("Begin USSD");
+		response.ServiceCode = strings.TrimPrefix(ussdReceive.Body.USSDReceiveNotifyUSSDReceptionBody.ServiceCode, "*")
+		response.ServiceCode = strings.TrimSuffix(ussdReceive.Body.USSDReceiveNotifyUSSDReceptionBody.ServiceCode, "#")
+		response.AccessCode = response.ServiceCode
+	} else {
+		response.AccessCode = ussdReceive.Body.USSDReceiveNotifyUSSDReceptionBody.ServiceCode
+		response.ServiceCode = ussdReceive.Body.USSDReceiveNotifyUSSDReceptionBody.ServiceCode
+	}
+	fmt.Println("USSD Receive From Exchange", ussdReceive)
+	return nil, response
 }
-
 
 func (request *ExchangeReceiveImplementation) ResolveClientResponse(c echo.Context, byteData []byte) error {
 	return c.XML(200, &exchange.USSDReceiveResponsePayload{
-		XmlNS: "http://schemas.xmlsoap.org/soap/envelope/",
+		XmlNS:   "http://schemas.xmlsoap.org/soap/envelope/",
 		XmlNLoc: "http://www.csapi.org/schema/parlayx/ussd/notification/v1_0/local",
-		Header: exchange.USSDReceiveResponseHeader{},
+		Header:  exchange.USSDReceiveResponseHeader{},
 		Body: exchange.USSDReceiveResponseBody{
 			USSDReceiveNotifyUSSDReceptionBody: exchange.USSDReceiveNotifyUSSDdReceptionResponse{
 				Result: "0",
@@ -77,5 +85,3 @@ func (request *ExchangeReceiveImplementation) ResolveClientResponse(c echo.Conte
 		},
 	})
 }
-
-
